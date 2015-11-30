4b7bdfb8abf85352643efb89d4d4b440c17629e6
Guillaume WOODWARD <gwoodwar@e1r1p10.42.fr>
2015-11-30 21:21:15 +0100

    last save

--- get_next_line.c
+++ get_next_line.c
@@ -6,48 +6,43 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 17:58:58 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/11/30 19:32:03 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/11/30 21:15:46 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
+#include "get_next_line.h"
 
-static int	is_in_lst(int const fd, t_dout *out, char **line)
+static t_list	*fd_lst(int const fd, t_list **lout)
 {
-	char *cursor;
-	while (*lout->next)
+	t_list	*nout;
+	t_out	*tmp;
+
+	while ((*lout)->next)
 	{
-		if (CAST(lout)->fdout == fd)
-		{
-			if (cursor = ft_strchr(CAST(lout)->bufout, '\n'))
-			{
-				*cursor = 0;
-				if cursor = BUFFSIZE
-				*(CAST(lout)->cursor) = '\0';
-				(CAST(lout)->cursor)++;
-				(CAST(lout)->bufout = ft_strcpy(CAST(lout)->bufout,
-												CAST(lout)->cursor));
-				*line = ft_strdup(CAST(lout)->bufout);
-				return (1);
-			}
-			else
-			{
-				*line = ft_strdup(CAST(lout)->bufout);
-				CAST(lout)->cursor += (ft_strlen(CAST(lout)->bufout));
-				return (0);
-			}
-		}
-		*lout = *lout->next;
+		if (CAST(*lout)->fdout == (int)fd)
+			return (*lout);
+		*lout = (*lout)->next;
 	}
-	return (-1);
+	if (!(nout = ft_lstnew(tmp, sizeof(t_out))))
+		return (NULL);
+	CAST(nout).fdout = fd;
+	CAST(nout)->bufout = NULL;
+	CAST(nout)->cursor = CAST(nout)->bufout;
+	ft_lstadd(lout, nout);
+	return (*lout);
 }
 
 static int		rd_lst(t_out *lout, char **line)
 {
-	char	*cursor;
-
-	if (cursor = ft_strchr(lout->bufout, '\n'))
+	if ((lout->cursor = ft_strchr(lout->bufout, '\n')))
 	{
-
+		*(lout->cursor) = 0;
+		if(!(*line = ft_strdup(lout->bufout)))
+			return (-1);
+		lout->bufout = ft_strcpy(lout->bufout, lout->cursor);
+		lout->cursor = lout->bufout;
+		return (1);
 	}
 	else
 	{
@@ -58,24 +53,30 @@ static int		rd_lst(t_out *lout, char **line)
 	}
 }
 
-static t_list	*fd_lst(int const fd, t_list **lout)
+static int		rd_fd(int const fd, char **line, t_out *lout)
 {
-	t_list	*nout;
-	t_out	*tmp;
+	int		ret;
+	char	buf[BUFF_SIZE];
+	char	*str;
+	char	*tmp;
 
-	while (*lout->next)
+	str = 0;
+	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		if (CAST(*lout).fdout == fd)
-			return (*lout);
-		*out = *lout->next;
+		buf[ret] = 0;
+		if (!(str = ft_strjoin(str, buf)))
+			return (-1);
+		tmp = str;
+		if (ft_strchr(tmp, '\n'))
+		{
+			if (!(lout->bufout = ft_strjoin(lout->bufout, tmp)))
+				return (-1);
+			*tmp = 0;
+			if (!(*line = ft_strjoin(*line, str)))
+			   return (-1);	
+		}
 	}
-	if (!(nout = ft_lstnew(tmp, sizeof(t_out))))
-		return (NULL);
-	CAST(nout).fdout = fd;
-	CAST(nout)->bufout = NULL;
-	CAST(nout)->cursor = CAST(nout)->bufout;
-	ft_lstadd(lout, nout);
-	return (lout);
+	return (ret);
 }
 
 int			get_next_line(int const fd, char **line)
@@ -85,7 +86,14 @@ int			get_next_line(int const fd, char **line)
 
 	if(!(*lout = fd_lst(fd, lout)))
 		return (-1);
-	if (i = rd_lst(CAST(*lout), line);
-
-	
+	if ((i = rd_lst(CAST(*lout), line)) == -1)
+		return (-1);
+	else if (i == 1)
+		return (1);
+	if ((i = rd_fd(fd, line, CAST(*lout))))
+		return (0);
+	else if (i == 1)
+		return (1);
+	else if (i == -1)
+		return (-1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 10:48:00 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/11/30 18:46:17 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/11/30 21:13:27 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 # define _GET_NEXT_LINE_H
 
 # define BUFF_SIZE			32
-# define CAST(x)			((t_dout*)x->content)
+# define CAST(x)			((t_out*)(x)->content)
 
 int					get_next_line(int const fd, char **line);
 
 typedef struct		s_out
 {
-	int const		fdout;
+	int				fdout;
 	char			*bufout;
 	char			*cursor;
 }					t_out;
