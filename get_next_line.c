ab6032e68c6326c8d6aeca18fd6e2c7198d8f8e8
Guillaume WOODWARD <gwoodwar@e1r5p19.42.fr>
2015-12-02 20:29:16 +0100

    save

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 17:58:58 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/12/02 19:45:10 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2015/12/02 20:18:16 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,25 +40,24 @@ static t_out	*fd_lst(int const fd, t_list **headptr)
 
 static int		rd_lst(t_out *out, char **line)
 {
-	if (*(out->bufout))
+	char	*tmp;
+
+	if (!*(out->bufout))
+		return (0);
+	if ((out->cursor = ft_strchr(out->bufout, '\n')))
 	{
-		if ((out->cursor = ft_strchr(out->bufout, '\n')))
-		{
-			*(out->cursor) = 0;
-			if (!(*line = ft_strjoin(*line, out->bufout)))
-				return (-1);
-			out->bufout = ft_strcpy(out->bufout, (out->cursor) + 1);
-			out->cursor = out->bufout;
-			return (1);
-		}
-		else
-		{
-			if (!(*line = ft_strjoin(*line, out->bufout)))
-				return (-1);
-			*(out->bufout) = 0;
-			return (0);
-		}
+		*(out->cursor) = 0;
+		if (!(*line = ft_strjoin(*line, out->bufout)))
+			return (-1);
+		out->bufout = ft_strcpy(out->bufout, (out->cursor) + 1);
+		out->cursor = out->bufout;
+		return (1);
 	}
+	tmp = *line;
+	if (!(*line = ft_strjoin(*line, out->bufout)))
+		return (-1);
+	free(tmp);
+	*(out->bufout) = 0;
 	return (0);
 }
 
@@ -79,22 +78,15 @@ int				get_next_line(int const fd, char **line)
 {
 	static t_list	*head = NULL;
 	t_out			*out;
-	int				i;
+	int				ret;
 
-	if (fd < 0 || !line)
+	if (fd < 0 || !line || BUFF_SIZE < 1)
 		return (-1);
 	*line = 0;
 	if (!(out = fd_lst(fd, &head)))
 		return (-1);
-	if ((i = rd_lst(out, line)) == -1)
-		return (-1);
-	else if (i == 1)
-		return (1);
-	if ((i = rd_fd(fd, line, out)) > 0)
-		return (1);
-	else if (i == 0)
-		return (0);
-	else if (i == -1)
-		return (-1);
-	return (0);
+	if ((ret = rd_lst(out, line)))
+		return (ret);
+	ret = rd_fd(fd, line, out);
+	return (ret);
 }
