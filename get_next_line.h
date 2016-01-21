d31357fadc9d77238dcd81fbf8276f0dc6ef615a
Guillaume WOODWARD <gwoodwar@e3r3p4.42.fr>
2016-01-21 15:12:50 +0100

    save ok yyang

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: gwoodwar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 10:48:00 by gwoodwar          #+#    #+#             */
-/*   Updated: 2015/12/03 10:56:46 by gwoodwar         ###   ########.fr       */
+/*   Updated: 2016/01/21 15:12:24 by gwoodwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _GET_NEXT_LINE_H
 # define _GET_NEXT_LINE_H
 
-# define BUFF_SIZE			2048
+# define BUFF_SIZE			10000
 # define CONTENT(x)			((t_out*)(x)->content)
 
 # include "libft/includes/libft.h"
